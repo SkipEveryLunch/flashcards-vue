fd4cd03f46bb9971e8d583a432ee5f6dcd7d7da5
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-06-21 23:00:56 +0900

    applied very simple style to Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,28 @@
+  
 <template>
-  <div>
-    This is a FlashCard App
-  </div>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-2xl md:text-3xl font-bold mb-3">
+      <i class="far fa-lightbulb"></i> 
+      {{appTitle}}
+    </div>
+
+    <p>
+      made by
+      <a class="text-blue-300" href="https://google.com" target="_blank"
+      >SkipEveryLunch</a
+      >
+    </p>
+  </header>
 </template>
 <script>
 export default {
-  name:"Header"
+  name:"Header",
+  setup(){
+    const appTitle = "Flash Cards App(temporary)";
+    return {
+      appTitle
+    };
+  }
 }
 </script>
 <style>
