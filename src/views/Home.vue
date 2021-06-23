b795d0aab7b596934e88301569f981965c65edc3
skipeverylunch <harima.yasuhiro@outlook.jp>
2021-06-23 22:59:31 +0900

    added super basic styling to Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,34 @@
 <template>
-  <div>
-    <div>
-    {{questions[progress].front}}
+  <div class="flex-col justify-between">
+    <div class="flex-col">
+      <div class="flex justify-center">
+        <div>
+      {{questions[progress].front}}
+        </div>
+      </div>
+      <div v-if="isAnswerShown"
+       class="flex justify-center">
+        <div>
+        {{questions[progress].back}}
+        </div>
+      </div>
     </div>
-    <div v-if="isAnswerShown">
-      {{questions[progress].back}}
+    <div class="flex justify-center">
+        <button
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        v-if="!isAnswerShown" @click="showAnswer">Answer</button>
+        <button 
+        class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        v-else @click="nextQuestion">
+          <span v-if="questions.length > progress + 1">
+          Next
+          </span>
+          <span v-else>
+            Done
+          </span>
+        </button>
+      </div>
     </div>
-    <div v-if="!isAnswerShown">
-      <button @click="showAnswer">Answer</button>
-    </div>
-    <div v-else>
-      <button @click="nextQuestion">
-        <span v-if="questions.length > progress + 1">
-        Next
-        </span>
-        <span v-else>
-          Done
-        </span>
-      </button>
-    </div>
-  </div>
 </template>
 
 <script lang="ts">
